dce0a7d873b0fa87d3262217fc2948ac3c7523b1
Kyncev <45149572+Kyncev@users.noreply.github.com>
2026-03-31 00:29:37 +0300

    Add files via upload

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+    public static String str1 = "This is shop-online app";
+
+    public static void main(String[] args) {
+        System.out.println(str1);
+    }
+}

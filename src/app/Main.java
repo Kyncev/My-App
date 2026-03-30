b8ed1a99e98db437bf59a1617c0edf3c8dadc84d
Kyncev <45149572+Kyncev@users.noreply.github.com>
2026-03-31 00:33:47 +0300

    Modified console text output

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,7 @@
 package app;
 
 public class Main {
-    public static String str1 = "This is shop-online app";
+    public static String str1 = "Shop-online for customers";
 
     public static void main(String[] args) {
         System.out.println(str1);
